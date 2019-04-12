0991eaab7f3399c64635d96577d4f16ffe941dde
Vladimir <vladimir.fodorow@gmail.com>
2019-04-12 17:56:20 +0700

    Рефракторинг тестов

--- src/test/java/SoundTest.java
+++ src/test/java/SoundTest.java
@@ -4,36 +4,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SoundTest {
 
+    byte[] bytes = new byte[]{
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    };
+
     @Test
     void CreateSoundTest() {
-        Sound sound = new Sound();
+        new Sound();
     }
 
     @Test
     void SetDataTest() {
         Sound sound = new Sound();
-        byte[] bytes = new byte[]{};
         sound.setData(bytes);
     }
 
     @Test
     void GetDataTest() {
         Sound sound = new Sound();
-        byte[] bytes = new byte[]{1};
         sound.setData(bytes);
-        assertArrayEquals(new byte[]{1}, sound.getData());
+        assertArrayEquals(bytes, sound.getData());
     }
 
     @Test
     void GetDataOnlyAudioTest() {
         Sound sound = new Sound();
-        byte[] bytes = new byte[]{
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        };
         sound.setData(bytes);
         assertArrayEquals(new byte[]{4, 5, 6, 7, 8, 9}, sound.getAudioData());
     }
@@ -41,23 +40,9 @@ class SoundTest {
     @Test
     void SculptTwoSounds() {
         Sound sound = new Sound();
-        byte[] bytes = new byte[]{
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        };
         sound.setData(bytes);
         Sound sound2 = new Sound();
-        byte[] bytes2 = new byte[]{
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-        };
-        sound2.setData(bytes2);
+        sound2.setData(bytes);
         sound.add(sound2);
         assertArrayEquals(new byte[]{
                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
