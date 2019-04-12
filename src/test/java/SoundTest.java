e24f76710d0749e6442ebf61cd27377021224b5b
FedorovVladimir <vladimir.fodorow@gmail.com>
2019-04-12 16:53:18 +0700

    Создан тест на дадание массива

--- src/test/java/SoundTest.java
+++ src/test/java/SoundTest.java
@@ -6,4 +6,11 @@ class SoundTest {
     void CreateSoundTest() {
         Sound sound = new Sound();
     }
+
+    @Test
+    void SetDataTest() {
+        Sound sound = new Sound();
+        byte[] bytes = new byte[]{};
+        sound.setData(bytes);
+    }
 }
